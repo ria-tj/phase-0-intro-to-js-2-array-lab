62dcace51aac62decf5614a5a49aa49a7d12f038
Ria <johnria6@gmail.com>
2024-07-08 13:54:37 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"] 
+
+const destructivelyAppendCat = (name) => {
+    cats.push(name);
+}
+
+const destructivelyPrependCat = (name => {
+    cats.unshift(name);
+})
+
+const destructivelyRemoveLastCat = (name) => {
+    {cats.pop();}
+}
+
+const appendCat = (name) => {
+    return [...cats, name];
+}
+
+const prependCat = (name) => {
+    return [name, ...cats]
+}
+
+const removeLastCat = (name) => {
+    return cats.slice(0, cats.length -1);
+}
+
+const removeFirstCat = (name) => {
+    return  cats.slice(1);
+}
+
+const destructivelyRemoveFirstCat = (name) => {
+    cats.shift();
+}
+  
